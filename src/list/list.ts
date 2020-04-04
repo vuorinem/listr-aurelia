5e7e6a59a25ea7c23ec5000bac73a7ade8eee413
Mikko Vuorinen <vuorinem@gmail.com>
2020-04-04 23:24:38 +0100

    Hook into reserve, cancel and list updates in list-api

--- src/list/list.ts
+++ src/list/list.ts
@@ -1,4 +1,4 @@
-import { ListData, getList, ItemData } from './list-api';
+import { ListData, getList, ItemData, Unsubscribe, subscribe, reserve, cancel } from './list-api';
 import { bindable } from 'aurelia';
 
 export class List {
@@ -6,16 +6,39 @@ export class List {
     name = '';
 
     list: ListData | null;
+    unsubscribe: Unsubscribe | null = null;
 
     async beforeBind() {
+        await this.initList();
+    }
+
+    async initList() {
+        await this.loadList();
+
+        if (this.unsubscribe !== null) {
+            this.unsubscribe();
+        }
+
+        this.unsubscribe = subscribe(this.name, () => this.loadList());
+    }
+
+    async loadList() {
         this.list = await getList(this.name);
     }
 
-    handleReserve(item: ItemData) {
-        alert(`Reserved '${item.label}'`);
+    async handleReserve(item: ItemData) {
+        if (!this.list) {
+            return;
+        }
+
+        await reserve(this.list.name, item.label);
     }
 
-    handleCancel(item: ItemData) {
-        alert(`Cancelled '${item.label}'`);
+    async handleCancel(item: ItemData) {
+        if (!this.list) {
+            return;
+        }
+
+        await cancel(this.list.name, item.label);
     }
 }
